8cdc05729d93c22f35b11823d8214fcb0eb8bfbe
jiiiyoung <156388828+jiiiyoung@users.noreply.github.com>
2024-07-06 14:47:41 +0900

    Update 홀짝 구분하기.java

--- 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.java
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.java
@@ -5,10 +5,14 @@ public class Solution {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         
+        // 삼항연산자 활용
+        System.out.println(n + " is " + ((n % 2==0) ? "even" : "odd"));
+        /*
         if(n % 2 == 0){
             System.out.println(n + " is even");
         }else{
             System.out.println(n + " is odd");
         }
+        */
     }
 }
