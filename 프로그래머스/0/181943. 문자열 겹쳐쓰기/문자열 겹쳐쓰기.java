3dd5b83251a5713979a2580ccbdd50b3407f987d
jiiiyoung <156388828+jiiiyoung@users.noreply.github.com>
2025-07-14 22:37:00 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.22 ms, Memory: 91.6 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -1,18 +1,17 @@
 class Solution {
     public String solution(String my_string, String overwrite_string, int s) {
-        String answer = "";
-        for(int i = 0; i < s;i++){
-            answer += my_string.toCharArray()[i];
+        StringBuilder result = new StringBuilder();
+        
+        for(int k = 0; k < my_string.length(); k++){
+            if(k >= s && k - s < overwrite_string.length() ){
+                result.append(overwrite_string.charAt(k-s));
+            }else{
+                result.append(my_string.charAt(k));
+            }
         }
         
-        for(int i = 0; i < overwrite_string.length(); i++){
-            answer += overwrite_string.toCharArray()[i];
-        }
-        int start = s + overwrite_string.length();
-        for(int i =start; i < my_string.length() ;i++){
-            answer += my_string.toCharArray()[i];
-        }
+        return result.toString();
+            
         
-        return answer;
     }
 }
